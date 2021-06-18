f6965721623f47898fa5ca24ce950404487cda8d
Vlad Sevostianov <flypsoff@gmail.com>
2021-06-18 10:50:14 +0300

    getting weather after init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'weather-forecast';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getWeather().subscribe();
+  }
 }
