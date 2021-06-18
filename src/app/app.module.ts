46f6babb73b7ba9b8cc74cdefb5fa48bc0834d42
Vlad Sevostianov <flypsoff@gmail.com>
2021-06-18 10:54:17 +0300

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWeatherComponent } from './components/main-weather/main-weather.component';
+import { HeaderWeatherComponent } from './components/header-weather/header-weather.component';
+import { InfoCardComponent } from './components/main-weather/info-card/info-card.component';
+import { DaysWeatherComponent } from './components/days-weather/days-weather.component';
+import { DayCardComponent } from './components/days-weather/day-card/day-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWeatherComponent,
+    HeaderWeatherComponent,
+    InfoCardComponent,
+    DaysWeatherComponent,
+    DayCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
